6e8d68684e962e1e2271d1c29024d0f4b7c99c3f
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-03-10 15:22:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count the characters/count-the-characters.java
+++ Difficulty: Basic/Count the characters/count-the-characters.java
@@ -35,25 +35,25 @@ class Sol
 {
     int getCount (String S, int N)
     {
-if(S.length()==0||N<1){
-            return 0;
-        }
-        
-        int[] freque=new int[26];
-        
-        for(int v=0;v<S.length();v++){
-            if(v>0&&S.charAt(v-1)==S.charAt(v)){
-               continue;
+       int c=0;
+       HashMap<Character,Integer> in=new HashMap<>();
+       int prev=0;
+       for(char curr: S.toCharArray())
+       {
+           
+           if(curr!=prev)
+           {
+               in.put(curr,in.getOrDefault(curr,0)+1);
+               
+           }
+           prev=curr;
+       }
+       for(Character x:in.keySet())
+       {
+            if(in.get(x)==N){
+                c++;
             }
-             int val=(int)S.charAt(v)-(int)'a';
-            freque[val]++;
-        }
-        
-        int count=0;
-        for(int v:freque){
-            if(v==N){
-                count++;
-            }
-        }
-        return count;    }
+       }
+            return c;
+    }
 }
