79b7bc2506ea11a73df13986f778fab972416d71
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-06-19 13:44:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Matrix Boundary Traversal/matrix-boundary-traversal.java
@@ -0,0 +1,24 @@
+class Solution {
+    static ArrayList<Integer> boundaryTraversal(int mat[][]) {
+        ArrayList<Integer> in = new ArrayList<>();
+        int n = mat.length, m = mat[0].length;
+
+        if (n == 1) {
+            for (int i = 0; i < m; i++) in.add(mat[0][i]);
+        }
+        else if (m == 1) {
+            for (int i = 0; i < n; i++) in.add(mat[i][0]);
+        }
+        else {
+            for (int i = 0; i < m; i++) in.add(mat[0][i]);
+
+            for (int i = 1; i < n; i++) in.add(mat[i][m - 1]);
+
+            for (int i = m - 2; i >= 0; i--) in.add(mat[n - 1][i]);
+
+            for (int i = n - 2; i >= 1; i--) in.add(mat[i][0]);
+        }
+
+        return in;
+    }
+}
